d67a32b63d2fe7675ad9f5e13f04a0c8b0d6c3b8
MiguelAngelLozadaTorrico <a2020110903@estudiantes.upsa.edu.bo>
2021-10-13 21:04:50 -0400

    Creacion del modelo

--- Lucky_miguellozada/Models/Suerte.cs
+++ Lucky_miguellozada/Models/Suerte.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Lucky_miguellozada.Models
+namespace Lucky_miguellozada.Models 
 {
     public class Suerte
     {
