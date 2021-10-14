b6c08e1955756be1b83d0d6edac485842005d90e
MiguelAngelLozadaTorrico <a2020110903@estudiantes.upsa.edu.bo>
2021-10-13 21:10:26 -0400

    Creacion del random controller

---
+++ Lucky_miguellozada/Migrations/20211014010742_AddModel.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Lucky_miguellozada.Migrations
+{
+    public partial class AddModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Suertesita",
+                columns: table => new
+                {
+                    SuerteId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Detalle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Imagen = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Suertesita", x => x.SuerteId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Suertesita");
+        }
+    }
+}
