a94fd708b0728d93a343b72ea3f2ecd8bff72641
MiguelAngelLozadaTorrico <a2020110903@estudiantes.upsa.edu.bo>
2021-10-13 21:04:27 -0400

    Primmet commit

---
+++ Lucky_miguellozada/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Lucky_miguellozada.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lucky_miguellozada.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Suerte> Suertesita { get; set; }
+        public object Suerte { get; internal set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
---
+++ Lucky_miguellozada/Models/Suerte.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lucky_miguellozada.Models
+{
+    public class Suerte
+    {
+
+        [Key]
+        [Display(Name = "Numero de la suerte")]
+        public int SuerteId { get; set; }
+        [Required]
+
+        [Display(Name = "Detalle de que te depara de suerte")]
+        public string Detalle { get; set; }
+
+        [Url]
+        [Display(Name = "Imagen")]
+        public string Imagen { get; set; }
+
+    }
+}
